81733af8c553b893e95ee7ee053771c730e15c81
YuriyBum <32727655+YuriyBum@users.noreply.github.com>
2018-05-06 15:37:53 +0500

    Create CryptiberLight.sol

---
+++ CryptiberLight.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.4.16;
+
+contract CryptiberLight {
+    
+    string Invention;
+    
+    function AddHash (string NewHash) public {
+        Invention = NewHash;
+            }
+    
+}
